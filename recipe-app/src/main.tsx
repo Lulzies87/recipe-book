e7f430e5a72c7f6d0821e60b6a6df7408451641c
Lilach E <lilachelayza@gmail.com>
2024-02-29 14:10:50 +0200

    Change index.css to index.scss

--- recipe-app/src/main.tsx
+++ recipe-app/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./router.ts";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
