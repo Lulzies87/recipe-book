b008db94673bc9694cd6aa253e1a437d0bb4f49d
Assaf Basri <Faskoman90@gmail.com>
2024-02-29 11:28:03 +0200

    sideBar styling and details>summary

--- recipe-app/src/Header.tsx
+++ recipe-app/src/Header.tsx
@@ -1,11 +1,16 @@
-import styles from "./Header.module.scss"
+import styles from "./Header.module.scss";
 
 export function Header() {
-
-    return (
-        <header className={styles.siteHeader}>
-            <h1 className={styles.siteHeader__title}>Recipe Book</h1>
-            <span className={styles.siteHeader__userInfo}><img className={styles.siteHeader__userInfo__avatar} src="https://i.pravatar.cc/" alt="avatar pic" /></span>
-        </header>
-    )
+  return (
+    <header className={styles.siteHeader}>
+      <h1 className={styles.siteHeader__title}>Recipe Book</h1>
+      <span className={styles.siteHeader__userInfo}>
+        <img
+          className={styles.siteHeader__userInfo__avatar}
+          src="https://i.pravatar.cc/"
+          alt="avatar pic"
+        />
+      </span>
+    </header>
+  );
 }
--- recipe-app/src/SideBar.tsx
+++ recipe-app/src/SideBar.tsx
@@ -1,8 +1,46 @@
-import styles from "./SideBar.module.scss"
+import styles from "./SideBar.module.scss";
 
 export function SideBar() {
-
-    return (
-        <h2 className={styles.sideBar}>Side bar</h2>
-    )
+  return (
+    <menu className={styles.sideBar}>
+      <ul className={styles.filters}>
+        <li className={styles.filters__listItem}>
+          <details className={styles.sideBar__details}>
+            Lorem ipsum
+            <summary>this</summary>
+          </details>
+        </li>
+        <li className={styles.filters__listItem}>
+          <details className={styles.sideBar__details}>
+            dolor sit amet consectetur adipisicing elit
+            <summary>that</summary>
+          </details>
+        </li>
+        <li className={styles.filters__listItem}>
+          <details className={styles.sideBar__details}>
+            Aspernatur, deleniti
+            <summary>and this</summary>
+          </details>
+        </li>
+        <li className={styles.filters__listItem}>
+          <details className={styles.sideBar__details}>
+            Lorem ipsum
+            <summary>this</summary>
+          </details>
+        </li>
+        <li className={styles.filters__listItem}>
+          <details className={styles.sideBar__details}>
+            dolor sit amet consectetur adipisicing elit
+            <summary>that</summary>
+          </details>
+        </li>
+        <li className={styles.filters__listItem}>
+          <details className={styles.sideBar__details}>
+            Aspernatur, deleniti
+            <summary>and this</summary>
+          </details>
+        </li>
+      </ul>
+    </menu>
+  );
 }
