eeda26ee16989184f842b253eef0394b34791ba8
Lilach E <lilachelayza@gmail.com>
2024-03-02 13:28:50 +0200

    Comments for Assaf

--- recipe-app/src/MainPage.tsx
+++ recipe-app/src/MainPage.tsx
@@ -7,7 +7,8 @@ import { Recipe } from "./Recipe.model";
 
 export function MainPage() {
   const recipes = useLoaderData() as Recipe[];
-
+  // Assaf#1: We're displaying 100 random recipes! :)
+  // After we get the "recipes" array from the loader, we pass it to the RecipesGrid + RecipeCard components using props.
   return (
     <>
       <Header />
--- recipe-app/src/RecipesGrid.tsx
+++ recipe-app/src/RecipesGrid.tsx
@@ -4,6 +4,9 @@ import styles from "./RecipesGrid.module.scss";
 
 type Props = { recipes: Recipe[] };
 
+// Assaf #2: Here we display a grid of recipe cards.
+// The grid dimentions are fixed, not suitable for various screen sizes yet.
+
 export function RecipesGrid({ recipes }: Props) {
   return (
     <ul className={styles.recipesGrid}>
