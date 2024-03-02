51513d027fbe16c406ecd4641f42f87e0f629dda
Lilach E <lilachelayza@gmail.com>
2024-03-02 10:04:11 +0200

    Import useLoaderData to MainPage.tsx

--- recipe-app/src/MainPage.tsx
+++ recipe-app/src/MainPage.tsx
@@ -1,9 +1,10 @@
+import { useLoaderData } from "react-router";
 import { Header } from "./Header";
 import { SideBar } from "./SideBar";
 
 export function MainPage() {
-  // const data = useLoaderData();
-  // console.log(data);
+  const data = useLoaderData();
+  console.log(data);
 
   return (
     <>
