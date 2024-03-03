5607565290034951107fac994024812a08696948
Lilach E <lilachelayza@gmail.com>
2024-03-03 15:21:39 +0200

    Add search bar (not functional)

--- recipe-app/src/SideBar.tsx
+++ recipe-app/src/SideBar.tsx
@@ -3,6 +3,9 @@ import styles from "./SideBar.module.scss";
 export function SideBar() {
   return (
     <menu className={styles.sideBar}>
+      <form name="searchRecipe">
+        <input className={styles.searchRecipe} id="search-recipe" type="search" name="search-recipe" />
+      </form>
       <ul className={styles.filters}>
         <li className={styles.filters__listItem}>
           <details className={styles.sideBar__details}>
