d8d00a77a1003f1237eaa4cebc7baa9704dc35ca
Lilach E <lilachelayza@gmail.com>
2024-03-04 14:12:16 +0200

    Define a baseURL in router

--- recipe-app/src/router.tsx
+++ recipe-app/src/router.tsx
@@ -5,6 +5,10 @@ import { MainPage } from "./MainPage";
 import { LoginPage } from "./LoginPage";
 import { Recipe } from "./Recipe.model";
 
+const server = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -14,14 +18,14 @@ export const router = createBrowserRouter([
         index: true,
         Component: MainPage,
         async loader() {
-          const response = await axios.get<{
+          const response = await server.get<{
             recipes: {
               id: string;
               title: string;
               image: string;
               readyInMinutes: number;
             }[];
-          }>("http://localhost:3000/");
+          }>("/");
 
           const recipes: Recipe[] = response.data.recipes.map(
             ({ id, title, image, readyInMinutes }) => ({
@@ -43,14 +47,14 @@ export const router = createBrowserRouter([
         path: "/:search",
         Component: MainPage,
         async loader({ params }) {
-          const response = await axios.get<{
+          const response = await server.get<{
             results: {
               id: string;
               title: string;
               image: string;
               readyInMinutes: number;
             }[];
-          }>(`http://localhost:3000/${params.search}`);
+          }>(`/${params.search}`);
 
           const recipes: Recipe[] = response.data.results.map(
             ({ id, title, image, readyInMinutes }) => ({
