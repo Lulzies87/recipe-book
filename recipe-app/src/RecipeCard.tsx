618fe0d071c5399669edc82f4fd87168e8fbefb4
Lilach E <lilachelayza@gmail.com>
2024-03-06 12:48:37 +0200

    Pass recipe.id to URL on click

--- recipe-app/src/RecipeCard.tsx
+++ recipe-app/src/RecipeCard.tsx
@@ -1,11 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 import { Recipe } from "./Recipe.model";
 import styles from "./RecipeCard.module.scss";
 
 type Props = { recipe: Recipe };
 
 export function RecipeCard({ recipe }: Props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const showRecipeDetails = () => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("recipe", recipe.id);
+    setSearchParams(newSearchParams);
+  };
+
   return (
-    <li className={styles.recipeCard}>
+    <li className={styles.recipeCard} onClick={showRecipeDetails}>
       <img
         className={styles.recipeImage}
         src={recipe.image}
