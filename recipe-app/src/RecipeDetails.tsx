56522c6d480a3f9a56d86eb157cd503ce2edaeda
Lilach E <lilachelayza@gmail.com>
2024-03-07 12:37:00 +0200

    Further design of RecipeDetails

--- recipe-app/src/RecipeDetails.tsx
+++ recipe-app/src/RecipeDetails.tsx
@@ -13,9 +13,19 @@ export function RecipeDetails({ recipes, recipeId }: Props) {
   return (
     <article className={styles.recipeDetails}>
       <h1 className={styles.recipeDetails__header}>{recipeDetails?.title}</h1>
+      <h2>Summary</h2>
       <p className={styles.recipeDetails__summary}>{recipeDetails?.summary}</p>
-      <p>Cooking time: {recipeDetails?.readyInMinutes}</p>
-      <img className={styles.recipeDetails__image} src={recipeDetails?.image} alt={recipeDetails?.title} />
+      <div className={styles.recipeDetails__statsContainer}>
+        <div className={styles.recipeDetails__statsContainer__stats}>
+          <p>Cooking time: {recipeDetails?.readyInMinutes}</p>
+          <p>Servings: {recipeDetails?.servings}</p>
+          <p>Health score: {recipeDetails?.servings}</p>
+        </div>
+        <img className={styles.recipeDetails__statsContainer__image} src={recipeDetails?.image} alt={recipeDetails?.title} />
+      </div>
+      <h2>Instructions</h2>
+      <p className={styles.recipeDetails__summary}>{recipeDetails?.instructions}</p>
+
     </article>
   );
 }
